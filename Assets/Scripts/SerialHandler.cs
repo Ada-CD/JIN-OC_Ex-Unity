4ff806ce7baac32988bc1814511d2328ed50a5ee
Ada-CD <32749416+Ada-CD@users.noreply.github.com>
2025-11-23 00:01:45 +0000

    Serial: gracefully handle disconnection and opening failure
    The SerialPort object provided by .NET is quite limited : it does not
    provide a way to check if the serial device is still present before trying
    to access it, and will not close a device failing to communicate.
    With the current script, this means that the we will just be spammed with
    exceptions in case of a deconnection.
    
    We also only try to connect once and will never try again, meaning that
    a deconnection or failed initial connection would force a restart.
    
    Implement a function to handle opening of the serial device, catching
    exceptions in case of failure so we can retry later.
    Update the port name, so it can be updated from the live inspector.
    Rate limit it so we can call it in `Update()` without too much worry
    or warning spam.
    
    Now that we can retry opening a serial connection, catch exceptions
    when reading or writing to it and manually close the connection in those
    cases.
    This allows the serial device to be plugged back in, correct the port
    in the inspector, or fix other issues, without Unity freezing, spamming
    errors, or needing to restart the game.

--- Assets/Scripts/SerialHandler.cs
+++ Assets/Scripts/SerialHandler.cs
@@ -11,6 +11,8 @@ public class SerialHandler : MonoBehaviour
 	// Common default serial device on a Windows machine
 	[SerializeField] private string serialPort;
 	[SerializeField] [Min(0)] private int baudrate;
+	private const float OpenAttemptDelay = 5;
+	private float _lastOpenAttemptTime = -OpenAttemptDelay;
 
 	[SerializeField] private Component river;
 	private Rigidbody2D _riverRigidbody2D;
@@ -27,23 +29,61 @@ public class SerialHandler : MonoBehaviour
 		_serial = new SerialPort(serialPort, baudrate);
 		// Guarantee that the newline is common across environments.
 		_serial.NewLine = "\n";
-		// Once configured, the serial communication must be opened just like a file : the OS handles the communication.
-		_serial.Open();
+		// Once configured, the serial communication must be opened.
+		// Just like a file or a socket : the OS handles the hard work.
+		OpenSerial();
+	}
+
+	// Try to open the configured serial port, rate limited by OpenAttemptDelay
+	// Return if we successfully opened.
+	private bool OpenSerial()
+	{
+		// Don't try to re-connect too often
+		if (Time.time < _lastOpenAttemptTime + OpenAttemptDelay) return false;
+		_lastOpenAttemptTime = Time.time;
+		
+		try {
+			// Allow updating the port name in the inspector while running the game.
+			_serial.PortName =  serialPort;
+			_serial.Open();
+		} catch (Exception e) {
+			Debug.LogWarning($"Could not open serial {serialPort} : {e.Message}");
+			return false;
+		}
+
+		Debug.Log($"Serial opened on {serialPort}");
+		return true;
+	}
+
+	private void CloseSerial()
+	{
+		_serial.Close();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		// Return early if not open, prevent spamming errors for no reason.
-		if (!_serial.IsOpen) return;
-		// Prevent blocking if no message is available as we are not doing anything else
-		// Alternative solutions : set a timeout, read messages in another thread, coroutines, futures...
-		if (_serial.BytesToRead <= 0) return;
-
+		// If the port is not open and could not be opened, we have nothing to do : return early.
+		if (!_serial.IsOpen && !OpenSerial()) return;
+		
+		string message;
+		// We might not know that the serial port has been disconnected :
+		// catch any errors and properly close it so we can recover.
+		try {
+			// Prevent blocking if no message is available as we are not doing anything else
+			// Alternative solutions : set a timeout, read messages in another thread, coroutines, futures...
+			if (_serial.BytesToRead <= 0) return;
+			
+			message = _serial.ReadLine();
+		} catch (Exception e) {
+			Debug.LogError($"Error reading serial : {e.Message}\nResetting connection.");
+			CloseSerial();
+			return;
+		}
+		
 		// Trim leading and trailing whitespaces makes it easier to handle different line endings.
 		// Arduino uses \r\n by default with `.println()`.
-		var message = _serial.ReadLine().Trim();
-
+		message = message.Trim();
 		// Split the message on spaces, so we can read the header and possible arguments.
 		var messageParts = message.Split(' ');
 		var header = messageParts[0];
@@ -85,7 +125,12 @@ public class SerialHandler : MonoBehaviour
 	public void SetLed(bool newState)
 	{
 		if (!_serial.IsOpen) return;
-		_serial.WriteLine(newState ? "LED ON" : "LED OFF");
+		try {
+			_serial.WriteLine(newState ? "LED ON" : "LED OFF");
+		} catch (Exception e) {
+			Debug.LogError($"Error writing to serial : {e.Message}\nResetting connection.");
+			CloseSerial();
+		}
 	}
 
 	private void OnDestroy()
